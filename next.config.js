99a65c64a60a4dc159c6bb847a8ceb75b0152a58
Abner Estrada <ajestrada@foragrosa.corp>
2023-03-30 08:19:01 -0600

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    unoptimized: true,
+    domains: ['firebasestorage.googleapis.com'],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/home/[index].tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+import PropTypes from 'prop-types'
+
+
+
+
+const HomePages:FC = props => {
+  return (
+    <div>HomePages</div>
+  )
+}
+
+HomePages.propTypes = {
+
+}
+
+export default HomePages
