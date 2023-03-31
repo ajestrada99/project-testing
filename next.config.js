e8fff7f0d49871d26c4c569f6af24c42514a9dd8
Abner Estrada <ajestrada@foragrosa.corp>
2023-03-31 08:43:10 -0600

    .env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
-  trailingSlash: true,
-  productionBrowserSourceMaps: true,
+  // reactStrictMode: true,
+  // experimental: {
+  //   appDir: false
+  // },
+  // trailingSlash: true,
+  // productionBrowserSourceMaps: true,
   images: {
     unoptimized: true,
     domains: ['firebasestorage.googleapis.com'],
