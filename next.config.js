deb1c68a93e813a4fa8f51ab997c60b53e3fa8d3
Abner Estrada <ajestrada@foragrosa.corp>
2023-03-31 09:33:03 -0600

    .env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
-  // experimental: {
-  //   appDir: false
-  // },
+   experimental: {
+     appDir: true
+   },
   // trailingSlash: true,
   // productionBrowserSourceMaps: true,
   images: {
